4283e24e98f3e97a4feb5b02ff4bf2b080bac7b7
Suneel <sunilsvsnlr@gmail.com>
2016-11-24 22:29:43 +0530

    test123

--- server.js
+++ server.js
@@ -76,7 +76,9 @@ router.get('/ValidateAdmin', function(req, res) {
 });
 
 router.post('/CreateUser', function(req, res) {
-     pg.connect(connectionstring, function (err, conn, done) {
+      res.json(req.body);
+    
+    /*pg.connect(connectionstring, function (err, conn, done) {
          if (err) console.log(err);
          conn.query('INSERT INTO UserManagement (firstname, lastname, username, email, phone, password) VALUES ($1, $2, $3, $4, $5, ,$6)',
                   [req.body.firstname.trim(), req.body.lastname.trim(), req.body.username.trim(), req.body.email.trim(), req.body.phone.trim(), req.body.password.trim()],
@@ -89,7 +91,7 @@ router.post('/CreateUser', function(req, res) {
                     res.json(result);
                 }
          });
-     });
+     });*/
 });
 
 router.get('/getUsers', function(req, res) {
