5f87cf821cc049e16431e625bff6fec410e084d4
Suneel <sunilsvsnlr@gmail.com>
2016-11-25 18:32:09 +0530

    test123

--- server.js
+++ server.js
@@ -98,8 +98,8 @@ router.get('/ValidateAdmin', function(req, res) {
 });
 
 router.post('/CreateUser', function(req, res) {
-    res.json(req.body);
-    res.json(JSON.parse(req.body));
+    res.json(req.body.trim());
+    //res.json(JSON.parse(req.body.trim()));
     
     /*pg.connect(connectionstring, function (err, conn, done) {
          if (err) console.log(err);
