7d81d1b1956ea705bfa0aecafa8f831a93da8821
Suneel <sunilsvsnlr@gmail.com>
2016-11-29 18:32:39 +0530

    test123

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.use(express.bodyParser({limit: '50mb'}));
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 5000);
 
