f6848bef6e32a95383be1b3e216039732fb1d97a
Suneel <sunilsvsnlr@gmail.com>
2016-12-01 13:14:19 +0530

    test123

--- server.js
+++ server.js
@@ -168,43 +168,74 @@ router.post('/insertCase', function(req, res) {
         
         //---------------Format Data---------------
         
-        jsonData.DeviceName = if(jsonData.DeviceName === null || jsonData.DeviceName === "null" || jsonData.DeviceName.length < 1) { NULL } else { ''+jsonData.DeviceName+'' };
-        jsonData.Dosage = if(jsonData.Dosage === null || jsonData.Dosage === "null" || jsonData.Dosage.length < 1) { NULL } else { ''+jsonData.Dosage+'' };
-        jsonData.DosageForm = if(jsonData.DosageForm === null || jsonData.DosageForm === "null" || jsonData.DosageForm.length < 1) { NULL } else { ''+jsonData.DosageForm+'' };
-        jsonData.BatchSerialNbr = if(jsonData.BatchSerialNbr === null || jsonData.BatchSerialNbr === "null" || jsonData.BatchSerialNbr.length < 1) { NULL } else { ''+jsonData.BatchSerialNbr+'' };
-        jsonData.Description = if(jsonData.Description === null || jsonData.Description === "null" || jsonData.Description.length < 1) { NULL } else { ''+jsonData.Description+'' };
-        jsonData.DateOfFirstUse = if(jsonData.DateOfFirstUse === null || jsonData.DateOfFirstUse === "null" || jsonData.DateOfFirstUse.length < 1) { NULL } else { ''+jsonData.DateOfFirstUse+'' };
-        jsonData.ExpiryDate = if(jsonData.ExpiryDate === null || jsonData.ExpiryDate === "null" || jsonData.ExpiryDate.length < 1) { NULL } else { ''+jsonData.ExpiryDate+'' };
-        jsonData.InitialPatientName = if(jsonData.InitialPatientName === null || jsonData.InitialPatientName === "null" || jsonData.InitialPatientName.length < 1) { NULL } else { ''+jsonData.InitialPatientName+'' };
-        jsonData.InitialPatientSurName = if(jsonData.InitialPatientSurName === null || jsonData.InitialPatientSurName === "null" || jsonData.InitialPatientSurName.length < 1) { NULL } else { ''+jsonData.InitialPatientSurName+'' };
-        jsonData.Age = if(jsonData.Age === null || jsonData.Age === "null" || jsonData.Age.length < 1) { NULL } else { jsonData.Age };
-        jsonData.Gender = if(jsonData.Gender === null || jsonData.Gender === "null" || jsonData.Gender.length < 1) { NULL } else { ''+jsonData.Gender+'' };
-        jsonData.QtyOfProductsConcerned = if(jsonData.QtyOfProductsConcerned === null || jsonData.QtyOfProductsConcerned === "null" || jsonData.QtyOfProductsConcerned.length < 1) { NULL } else { jsonData.QtyOfProductsConcerned };
-        jsonData.NameOfCompliant = if(jsonData.NameOfCompliant === null || jsonData.NameOfCompliant === "null" || jsonData.NameOfCompliant.length < 1) { NULL } else { ''+jsonData.NameOfCompliant+'' };
-        jsonData.DefectDescription = if(jsonData.DefectDescription === null || jsonData.DefectDescription === "null" || jsonData.DefectDescription.length < 1) { NULL } else { ''+jsonData.DefectDescription+'' };
-        jsonData.IsComplaintSampleAvailable = if(jsonData.IsComplaintSampleAvailable === null || jsonData.IsComplaintSampleAvailable === "null" || jsonData.IsComplaintSampleAvailable.length < 1) { NULL } else { jsonData.IsComplaintSampleAvailable };
-        jsonData.ExpectedDateOfSampleReceived = if(jsonData.ExpectedDateOfSampleReceived === null || jsonData.ExpectedDateOfSampleReceived === "null" || jsonData.ExpectedDateOfSampleReceived.length < 1) { NULL } else { ''+jsonData.ExpectedDateOfSampleReceived+'' };
-        jsonData.HasResponseBeenRequested = if(jsonData.HasResponseBeenRequested === null || jsonData.HasResponseBeenRequested === "null" || jsonData.HasResponseBeenRequested.length < 1) { NULL } else { jsonData.HasResponseBeenRequested };
-        jsonData.IsPatientFamiliarWithDeviceUsage = if(jsonData.IsPatientFamiliarWithDeviceUsage === null || jsonData.IsPatientFamiliarWithDeviceUsage === "null" || jsonData.IsPatientFamiliarWithDeviceUsage.length < 1) { NULL } else { jsonData.IsPatientFamiliarWithDeviceUsage };
-        jsonData.SinceWhenPatientUseThisDevice = if(jsonData.SinceWhenPatientUseThisDevice === null || jsonData.SinceWhenPatientUseThisDevice === "null" || jsonData.SinceWhenPatientUseThisDevice.length < 1) { NULL } else { ''+jsonData.SinceWhenPatientUseThisDevice+'' };
-        jsonData.IsDevicePhysicallyDamaged = if(jsonData.IsDevicePhysicallyDamaged === null || jsonData.IsDevicePhysicallyDamaged === "null" || jsonData.IsDevicePhysicallyDamaged.length < 1) { NULL } else { jsonData.IsDevicePhysicallyDamaged };
-        jsonData.Where = if(jsonData.Where === null || jsonData.Where === "null" || jsonData.Where.length < 1) { NULL } else { ''+jsonData.Where+'' };
-        jsonData.DamageDuetoAccidentalFall = if(jsonData.DamageDuetoAccidentalFall === null || jsonData.DamageDuetoAccidentalFall === "null" || jsonData.DamageDuetoAccidentalFall.length < 1) { NULL } else { jsonData.DamageDuetoAccidentalFall };
-        jsonData.FromWhichHeightOccuredtheFall = if(jsonData.FromWhichHeightOccuredtheFall === null || jsonData.FromWhichHeightOccuredtheFall === "null" || jsonData.FromWhichHeightOccuredtheFall.length < 1) { NULL } else { ''+jsonData.FromWhichHeightOccuredtheFall+'' };
-        jsonData.IsDefectedDuetomisusebypatient = if(jsonData.IsDefectedDuetomisusebypatient === null || jsonData.IsDefectedDuetomisusebypatient === "null" || jsonData.IsDefectedDuetomisusebypatient.length < 1) { NULL } else { jsonData.IsDefectedDuetomisusebypatient };
-        jsonData.Whichkindofmisuse = if(jsonData.Whichkindofmisuse === null || jsonData.Whichkindofmisuse === "null" || jsonData.Whichkindofmisuse.length < 1) { NULL } else { ''+jsonData.Whichkindofmisuse+'' };
-        jsonData.IsSomethingstuckinsidedevice = if(jsonData.IsSomethingstuckinsidedevice === null || jsonData.IsSomethingstuckinsidedevice === "null" || jsonData.IsSomethingstuckinsidedevice.length < 1) { NULL } else { jsonData.IsSomethingstuckinsidedevice };
-        jsonData.WhatStuckinside = if(jsonData.WhatStuckinside === null || jsonData.WhatStuckinside === "null" || jsonData.WhatStuckinside.length < 1) { NULL } else { ''+jsonData.WhatStuckinside+'' };
-        jsonData.Adverseeventassociatedtodefect = if(jsonData.Adverseeventassociatedtodefect === null || jsonData.Adverseeventassociatedtodefect === "null" || jsonData.Adverseeventassociatedtodefect.length < 1) { NULL } else { jsonData.Adverseeventassociatedtodefect };
-        jsonData.Adverseeventassociatedwithdefect = if(jsonData.Adverseeventassociatedwithdefect === null || jsonData.Adverseeventassociatedwithdefect === "null" || jsonData.Adverseeventassociatedwithdefect.length < 1) { NULL } else { ''+jsonData.Adverseeventassociatedwithdefect+'' };
-        jsonData.OtherInformation = if(jsonData.OtherInformation === null || jsonData.OtherInformation === "null" || jsonData.OtherInformation.length < 1) { NULL } else { ''+jsonData.OtherInformation+'' };
-        jsonData.Isproductcartridgestuckedindevice = if(jsonData.Isproductcartridgestuckedindevice === null || jsonData.Isproductcartridgestuckedindevice === "null" || jsonData.Isproductcartridgestuckedindevice.length < 1) { NULL } else { jsonData.Isproductcartridgestuckedindevice };
-        jsonData.Isreplacementofproductrequested = if(jsonData.Isreplacementofproductrequested === null || jsonData.Isreplacementofproductrequested === "null" || jsonData.Isreplacementofproductrequested.length < 1) { NULL } else { jsonData.Isreplacementofproductrequested };
-        jsonData.Subject = if(jsonData.Subject === null || jsonData.Subject === "null" || jsonData.Subject.length < 1) { NULL } else { ''+jsonData.Subject+'' };
-        jsonData.Priority = if(jsonData.Priority === null || jsonData.Priority === "null" || jsonData.Priority.length < 1) { NULL } else { ''+jsonData.Priority+'' };
-        jsonData.Status = if(jsonData.Status === null || jsonData.Status === "null" || jsonData.Status.length < 1) { NULL } else { ''+jsonData.Status+'' };
-        jsonData.userid = if(jsonData.userid === null || jsonData.userid === "null" || jsonData.userid.length < 1) { NULL } else { ''+jsonData.userid+'' };
-        jsonData.username = if(jsonData.username === null || jsonData.username === "null" || jsonData.username.length < 1) { NULL } else { ''+jsonData.username+'' };
+        if(jsonData.DeviceName === null || jsonData.DeviceName === "null" || jsonData.DeviceName.length < 1) { jsonData.DeviceName = NULL } else { jsonData.DeviceName = ''+jsonData.DeviceName+'' };
+        
+        if(jsonData.Dosage === null || jsonData.Dosage === "null" || jsonData.Dosage.length < 1) { jsonData.Dosage = NULL } else { jsonData.Dosage = ''+jsonData.Dosage+'' };
+        
+        if(jsonData.DosageForm === null || jsonData.DosageForm === "null" || jsonData.DosageForm.length < 1) { jsonData.DosageForm = NULL } else { jsonData.DosageForm = ''+jsonData.DosageForm+'' };
+        
+        if(jsonData.BatchSerialNbr === null || jsonData.BatchSerialNbr === "null" || jsonData.BatchSerialNbr.length < 1) { jsonData.BatchSerialNbr = NULL } else { jsonData.BatchSerialNbr = ''+jsonData.BatchSerialNbr+'' };
+        
+        if(jsonData.Description === null || jsonData.Description === "null" || jsonData.Description.length < 1) { jsonData.Description = NULL } else { jsonData.Description = ''+jsonData.Description+'' };
+        
+        if(jsonData.DateOfFirstUse === null || jsonData.DateOfFirstUse === "null" || jsonData.DateOfFirstUse.length < 1) { jsonData.DateOfFirstUse = NULL } else { jsonData.DateOfFirstUse = ''+jsonData.DateOfFirstUse+'' };
+        
+        if(jsonData.ExpiryDate === null || jsonData.ExpiryDate === "null" || jsonData.ExpiryDate.length < 1) { jsonData.ExpiryDate = NULL } else { jsonData.ExpiryDate = ''+jsonData.ExpiryDate+'' };
+        
+        if(jsonData.InitialPatientName === null || jsonData.InitialPatientName === "null" || jsonData.InitialPatientName.length < 1) { jsonData.InitialPatientName = NULL } else { jsonData.InitialPatientName = ''+jsonData.InitialPatientName+'' };
+        
+        if(jsonData.InitialPatientSurName === null || jsonData.InitialPatientSurName === "null" || jsonData.InitialPatientSurName.length < 1) { jsonData.InitialPatientSurName = NULL } else { jsonData.InitialPatientSurName = ''+jsonData.InitialPatientSurName+'' };
+        
+        if(jsonData.Age === null || jsonData.Age === "null" || jsonData.Age.length < 1) { jsonData.Age = NULL } else { jsonData.Age = jsonData.Age };
+        if(jsonData.Gender === null || jsonData.Gender === "null" || jsonData.Gender.length < 1) { jsonData.Gender = NULL } else { jsonData.Gender = ''+jsonData.Gender+'' };
+        
+        if(jsonData.QtyOfProductsConcerned === null || jsonData.QtyOfProductsConcerned === "null" || jsonData.QtyOfProductsConcerned.length < 1) { jsonData.QtyOfProductsConcerned = NULL } else { jsonData.QtyOfProductsConcerned = jsonData.QtyOfProductsConcerned };
+        
+        if(jsonData.NameOfCompliant === null || jsonData.NameOfCompliant === "null" || jsonData.NameOfCompliant.length < 1) { jsonData.NameOfCompliant = NULL } else { jsonData.NameOfCompliant = ''+jsonData.NameOfCompliant+'' };
+        
+        if(jsonData.DefectDescription === null || jsonData.DefectDescription === "null" || jsonData.DefectDescription.length < 1) { jsonData.DefectDescription = NULL } else { jsonData.DefectDescription = ''+jsonData.DefectDescription+'' };
+        
+        if(jsonData.IsComplaintSampleAvailable === null || jsonData.IsComplaintSampleAvailable === "null" || jsonData.IsComplaintSampleAvailable.length < 1) { jsonData.IsComplaintSampleAvailable = NULL } else { jsonData.IsComplaintSampleAvailable = jsonData.IsComplaintSampleAvailable };
+        
+        if(jsonData.ExpectedDateOfSampleReceived === null || jsonData.ExpectedDateOfSampleReceived === "null" || jsonData.ExpectedDateOfSampleReceived.length < 1) { jsonData.ExpectedDateOfSampleReceived = NULL } else { jsonData.ExpectedDateOfSampleReceived = ''+jsonData.ExpectedDateOfSampleReceived+'' };
+        
+        if(jsonData.HasResponseBeenRequested === null || jsonData.HasResponseBeenRequested === "null" || jsonData.HasResponseBeenRequested.length < 1) { jsonData.HasResponseBeenRequested = NULL } else { jsonData.HasResponseBeenRequested = jsonData.HasResponseBeenRequested };
+        
+        if(jsonData.IsPatientFamiliarWithDeviceUsage === null || jsonData.IsPatientFamiliarWithDeviceUsage === "null" || jsonData.IsPatientFamiliarWithDeviceUsage.length < 1) { jsonData.IsPatientFamiliarWithDeviceUsage = NULL } else { jsonData.IsPatientFamiliarWithDeviceUsage = jsonData.IsPatientFamiliarWithDeviceUsage };
+        
+        if(jsonData.SinceWhenPatientUseThisDevice === null || jsonData.SinceWhenPatientUseThisDevice === "null" || jsonData.SinceWhenPatientUseThisDevice.length < 1) { jsonData.SinceWhenPatientUseThisDevice = NULL } else { jsonData.SinceWhenPatientUseThisDevice = ''+jsonData.SinceWhenPatientUseThisDevice+'' };
+        
+        if(jsonData.IsDevicePhysicallyDamaged === null || jsonData.IsDevicePhysicallyDamaged === "null" || jsonData.IsDevicePhysicallyDamaged.length < 1) { jsonData.IsDevicePhysicallyDamaged = NULL } else { jsonData.IsDevicePhysicallyDamaged = jsonData.IsDevicePhysicallyDamaged };
+        
+        if(jsonData.Where === null || jsonData.Where === "null" || jsonData.Where.length < 1) { jsonData.Where = NULL } else { jsonData.Where = ''+jsonData.Where+'' };
+        
+        if(jsonData.DamageDuetoAccidentalFall === null || jsonData.DamageDuetoAccidentalFall === "null" || jsonData.DamageDuetoAccidentalFall.length < 1) { jsonData.DamageDuetoAccidentalFall = NULL } else { jsonData.DamageDuetoAccidentalFall = jsonData.DamageDuetoAccidentalFall };
+        
+        if(jsonData.FromWhichHeightOccuredtheFall === null || jsonData.FromWhichHeightOccuredtheFall === "null" || jsonData.FromWhichHeightOccuredtheFall.length < 1) { jsonData.FromWhichHeightOccuredtheFall = NULL } else { jsonData.FromWhichHeightOccuredtheFall = ''+jsonData.FromWhichHeightOccuredtheFall+'' };
+        
+        if(jsonData.IsDefectedDuetomisusebypatient === null || jsonData.IsDefectedDuetomisusebypatient === "null" || jsonData.IsDefectedDuetomisusebypatient.length < 1) { jsonData.IsDefectedDuetomisusebypatient = NULL } else { jsonData.IsDefectedDuetomisusebypatient = jsonData.IsDefectedDuetomisusebypatient };
+        
+        if(jsonData.Whichkindofmisuse === null || jsonData.Whichkindofmisuse === "null" || jsonData.Whichkindofmisuse.length < 1) { jsonData.Whichkindofmisuse = NULL } else { jsonData.Whichkindofmisuse = ''+jsonData.Whichkindofmisuse+'' };
+        
+        if(jsonData.IsSomethingstuckinsidedevice === null || jsonData.IsSomethingstuckinsidedevice === "null" || jsonData.IsSomethingstuckinsidedevice.length < 1) { jsonData.IsSomethingstuckinsidedevice = NULL } else { jsonData.IsSomethingstuckinsidedevice = jsonData.IsSomethingstuckinsidedevice };
+        
+        if(jsonData.WhatStuckinside === null || jsonData.WhatStuckinside === "null" || jsonData.WhatStuckinside.length < 1) { jsonData.WhatStuckinside = NULL } else { jsonData.WhatStuckinside = ''+jsonData.WhatStuckinside+'' };
+        
+        if(jsonData.Adverseeventassociatedtodefect === null || jsonData.Adverseeventassociatedtodefect === "null" || jsonData.Adverseeventassociatedtodefect.length < 1) { jsonData.Adverseeventassociatedtodefect = NULL } else { jsonData.Adverseeventassociatedtodefect = jsonData.Adverseeventassociatedtodefect };
+        
+        if(jsonData.Adverseeventassociatedwithdefect === null || jsonData.Adverseeventassociatedwithdefect === "null" || jsonData.Adverseeventassociatedwithdefect.length < 1) { jsonData.Adverseeventassociatedwithdefect = NULL } else { jsonData.Adverseeventassociatedwithdefect = ''+jsonData.Adverseeventassociatedwithdefect+'' };
+        
+        if(jsonData.OtherInformation === null || jsonData.OtherInformation === "null" || jsonData.OtherInformation.length < 1) { jsonData.OtherInformation = NULL } else { jsonData.OtherInformation = ''+jsonData.OtherInformation+'' };
+        
+        if(jsonData.Isproductcartridgestuckedindevice === null || jsonData.Isproductcartridgestuckedindevice === "null" || jsonData.Isproductcartridgestuckedindevice.length < 1) { jsonData.Isproductcartridgestuckedindevice = NULL } else { jsonData.Isproductcartridgestuckedindevice = jsonData.Isproductcartridgestuckedindevice };
+        
+        if(jsonData.Isreplacementofproductrequested === null || jsonData.Isreplacementofproductrequested === "null" || jsonData.Isreplacementofproductrequested.length < 1) { jsonData.Isreplacementofproductrequested = NULL } else { jsonData.Isreplacementofproductrequested = jsonData.Isreplacementofproductrequested };
+        
+        if(jsonData.Subject === null || jsonData.Subject === "null" || jsonData.Subject.length < 1) { jsonData.Subject = NULL } else { jsonData.Subject = ''+jsonData.Subject+'' };
+        if(jsonData.Priority === null || jsonData.Priority === "null" || jsonData.Priority.length < 1) { jsonData.Priority = NULL } else { jsonData.Priority = ''+jsonData.Priority+'' };
+        if(jsonData.Status === null || jsonData.Status === "null" || jsonData.Status.length < 1) { jsonData.Status = NULL } else { jsonData.Status = ''+jsonData.Status+'' };
+        if(jsonData.userid === null || jsonData.userid === "null" || jsonData.userid.length < 1) { jsonData.userid = NULL } else { jsonData.userid = ''+jsonData.userid+'' };
+        if(jsonData.username === null || jsonData.username === "null" || jsonData.username.length < 1) { jsonData.username = NULL } else { jsonData.username = ''+jsonData.username+'' };
         
         //---------------End Format Data---------------
         
