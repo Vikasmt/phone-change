87368d7f109a18ed429fd8bea6a97fa67838ef10
Suneel <sunilsvsnlr@gmail.com>
2016-12-06 20:48:10 +0530

    test123

--- server.js
+++ server.js
@@ -375,7 +375,8 @@ router.get('/showImage', function(req, res) {
                 }
                 else{
                     var contenttype = result.rows[0].contenttype;
-                    contenttype = contenttype.replaceAll('"','');
+                    contenttype = contenttype.replace('"','');
+                    contenttype = contenttype.replace('"','');
                     console.log(contenttype);
                     var img = new Buffer(result.rows[0].body, 'base64');
                     res.writeHead(200, {
