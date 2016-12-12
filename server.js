5b962dfa2acac299526bab4752ac8060f0866804
vikas <vikas.y@moldtekindia.com>
2016-12-12 12:22:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,34 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+var router = express.Router();
+
+router.post('/CreateUser', function(req, res) {
+    console.log(req.body);
+    var jsonData = req.body;
+    
+    var formattedData='INSERT INTO UserManagement (firstname, email, phone, password) VALUES (\''+jsonData.name+'\', \''+jsonData.email+'\', 1234567899, \''+jsonData.password+'\')';
+    console.log('formattedQuery:'+formattedData);
+    
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+         if (err) console.log(err);
+         conn.query('INSERT INTO UserManagement (firstname, email, phone, password) VALUES (\''+jsonData.name+'\', \''+jsonData.email+'\', 1234567899, \''+jsonData.password+'\')',
+             function(err, result) {
+                done(); 
+             if(err){
+                    return res.json({
+                            msgid: 2,
+                            message: err.message});
+                }
+                else{
+                    return res.json({
+                            msgid: 1,
+                            message: 'Success.'});
+                }
+         });
+     });
+});
+
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
