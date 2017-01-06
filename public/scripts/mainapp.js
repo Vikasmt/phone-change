eae311ab094348bf8149c49ae159d77c9aa849cf
Suneel <sunilsvsnlr@gmail.com>
2017-01-06 21:58:45 +0530

    test123

--- public/scripts/mainapp.js
+++ public/scripts/mainapp.js
@@ -38,7 +38,7 @@
         $urlRouterProvider.otherwise("/");
         $stateProvider
             .state('home', {
-                url: '/home',
+                url: '/',
                 templateUrl: 'index.html',
                 controller: 'mainCtrl',
                 controllerAs: 'vm'
