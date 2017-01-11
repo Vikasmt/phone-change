b82cab6178e16c45f44453b4f7e1c30fe82b4c85
vikas <vikas.y@moldtekindia.com>
2017-01-11 15:08:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -609,7 +609,7 @@ function sendEmail(toemail, currentpassword){
     var mailOptions = {
         to: toemail,
         subject: 'FMA - Finished Reset Password',
-        text: 'Merck feedback managemant app recently received a request to reset the password for your user.\nPlease use this current password to login:'+currentpassword
+        text: 'Merck feedback managemant app recently received a request to reset the password.\n\nPlease use this current password to login: <b> '+currentpassword+'</b>';
         
     };
     
