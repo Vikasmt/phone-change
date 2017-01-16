59acda6edaff6f7c2a4e9b0870eb21dc223616ec
vikas <vikas.y@moldtekindia.com>
2017-01-16 17:25:44 +0530

    Update createuser.js

--- public/scripts/controllers/createuser.js
+++ public/scripts/controllers/createuser.js
@@ -1,7 +1,28 @@
 (function(){
 var app = angular.module("mainApp");
 app.controller("createCtrl", function($scope, $http, $state, $stateParams, apiUrl) {
-     
+     var name = $('input[name="name"]').val();
+if (name.length < 3)
+{
+    alert('Please enter a name 3 characters or more.');
+    return false;
+}
+//validate email
+var email = $('input[name="email"]').val(),
+    emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+if(!emailReg.test(email) || email == '')
+{
+     alert('Please enter a valid email address.');
+     return false;
+}
+//validate phone
+var phone = $('input[name="phone"]').val(),
+    intRegex = ([0-9]{10})|(\([0-9]{3}\)\s+[0-9]{3}\-[0-9]{4});
+if((phone.length < 6) || (!intRegex.test(phone)))
+{
+     alert('Please enter a valid phone number.');
+     return false;
+}
         $scope.user={};
       
         $scope.bindParameters = function(){
