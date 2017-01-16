97f39be193c1be2fb1ebc9159ca523d63b582f58
vikas <vikas.y@moldtekindia.com>
2017-01-16 17:08:16 +0530

    Update createuser.js

--- public/scripts/controllers/createuser.js
+++ public/scripts/controllers/createuser.js
@@ -1,8 +1,9 @@
 (function(){
 var app = angular.module("mainApp");
 app.controller("createCtrl", function($scope, $http, $state, $stateParams, apiUrl) {
+          $scope.phoneNumbr = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
         $scope.user={};
-        $scope.phoneNumbr = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
+      
         $scope.bindParameters = function(){
             $scope.user = angular.isDefined($stateParams.userdata) ? $stateParams.userdata : {};
             console.log($scope.user);
