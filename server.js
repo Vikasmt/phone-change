3b28563dc903e827df6fabd931627f98f5b8214d
vikas <vikas.y@moldtekindia.com>
2017-02-22 16:02:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -457,7 +457,7 @@ router.get('/getProducts', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         if (err) console.log(err);
         conn.query(
-            'SELECT count(id) AS sfcount, sfid, Name, Product_Description__c, Product_Type__c, Serial_Batch_code__c,Type__c, Product_Germany_Description__c, Product_Italy_Description__c, BrandingColor__c FROM salesforce.FMA_Product__c WHERE Type__c=\''+type+'\' GROUP BY sfid,Name,Product_Description__c, Product_Type__c, Serial_Batch_code__c,Type__c, Product_Germany_Description__c, Product_Italy_Description__c, BrandingColor__c',
+            'SELECT sfid, Name, Product_Description__c, Product_Type__c, Serial_Batch_code__c,Type__c, Product_Germany_Description__c, Product_Italy_Description__c, BrandingColor__c FROM salesforce.FMA_Product__c WHERE Type__c=\''+type+'\'',
             function(err,result){
                 done();
                 if(err){
@@ -468,6 +468,26 @@ router.get('/getProducts', function(req, res) {
                     for(var i=0; i<result.rows.length; i++){
                         result.rows[i].imageUrl = baseUrl + 'api/showImage?imageid='+result.rows[i].sfid+'&fromloc=Product';
                     }
+                    return res.json(result.rows);
+                }
+            });
+    });
+});
+
+router.get('/getProductsCount', function(req, res) {
+	var type;
+	if(req.param('producttype') === 'Medical') type = 'Medical Device';
+	if(req.param('producttype') === 'Combination') type = 'Combination Product';
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        if (err) console.log(err);
+        conn.query(
+            'SELECT count(id) AS sfCount FROM salesforce.FMA_Product__c WHERE Type__c=\''+type+'\' GROUP BY Id',
+            function(err,result){
+                done();
+                if(err){
+                   return res.status(400).json({error: err.message});
+                }
+                else{
                     return res.json(result.rows);
                 }
             });
