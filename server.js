fce643f2062855080cb554bf5e94f2b50e5e76aa
vikas <vikas.y@moldtekindia.com>
2017-02-27 18:04:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -499,7 +499,7 @@ router.get('/myFeedbacks', function(req, res) {
      pg.connect(process.env.DATABASE_URL, function (err, conn, done){
           if (err) console.log(err);
          conn.query(
-             'SELECT id, CaseNumber, Description, createddate, Status from salesforce.case order by createddate desc limit 10',
+             'SELECT sfid, casenumber, description, createddate, status from salesforce.case order by createddate desc limit 10',
              function(err,result){
                 done();
                 if(err){
