7a9e7ae6d6f4c6c5c31b6d2b5e516d047e76d5da
vikas <vikas.y@moldtekindia.com>
2017-02-27 19:05:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -457,7 +457,7 @@ router.get('/getProducts', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         if (err) console.log(err);
         conn.query(
-            'SELECT sfid, Name, Product_Type__c, Type__c, Product_Description__c, Product_Italy_Description__c, Serial_Batch_code__c, Serial_Batch_code_format__c, Serial_Batch_code_contains__c, BrandingColor__c FROM salesforce.FMA_Product__c WHERE Type__c=\''+type+'\'',
+            'SELECT sfid, Name, Product_Type__c, Type__c, Product_Description__c, Product_Italy_Description__c, Product_Germany_Description__c, Serial_Batch_code__c, Serial_Batch_code_format__c, Serial_Batch_code_contains__c, BrandingColor__c FROM salesforce.FMA_Product__c WHERE Type__c=\''+type+'\'',
             function(err,result){
                 done();
                 if(err){
