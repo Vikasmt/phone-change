27f6b73fbc9368f452f14fb580f5951ba3e7a36c
vikas <vikas.y@moldtekindia.com>
2017-03-21 18:41:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ router.get('/getDisclaimercontent', function(req, res) {
         if (err) console.log(err);
         conn.query(
             'SELECT id, country, language, content from disclaimercontent where country= \''+country+'\' and language= \''+language+'\'',
-	    [res.body.country.trim(), res.body.language.trim(), res.body.content.trim()],
+	   // [res.body.country.trim(), res.body.language.trim(), res.body.content.trim()],
             function(err,result){
                 done();
                 if(err){
