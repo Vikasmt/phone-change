56d11f5045e58b87e60bc7b619cb362f1e9134df
vikas <vikas.y@moldtekindia.com>
2017-03-23 15:32:20 +0530

    Update HelpList.js

--- public/scripts/controllers/HelpList.js
+++ public/scripts/controllers/HelpList.js
@@ -28,7 +28,7 @@ app.controller("HelpCtrl", function($scope, $http, $state, apiUrl) {
                 });
         };
     
-        $scope.redirectToEdit = function(HelpInfo){
+        /*$scope.redirectToEdit = function(HelpInfo){
             $state.go('createhelp', {'helpdata':HelpInfo, 'mode':'E'});
         }
         
@@ -52,7 +52,7 @@ app.controller("HelpCtrl", function($scope, $http, $state, apiUrl) {
                         console.log(data);
                     });
             }
-        };
+        };*/
     
         
     });    
