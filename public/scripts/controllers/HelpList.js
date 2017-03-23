6803baf4b35c5cdf67654685db03c25467298d38
vikas <vikas.y@moldtekindia.com>
2017-03-23 12:48:29 +0530

    Update HelpList.js

--- public/scripts/controllers/HelpList.js
+++ public/scripts/controllers/HelpList.js
@@ -1,75 +1,52 @@
 (function(){
 var app = angular.module("mainApp");
-app.controller("userCtrl", function($scope, $http, $state, apiUrl) {
-        $scope.userlist = [];
-        $scope.usersMainList = [];
+app.controller("HelpCtrl", function($scope, $http, $state, apiUrl) {
+        $scope.helplist = [];
+        $scope.helpMainList = [];
     
-        $scope.getUsers = function () {
+        $scope.getHelp = function () {
             $scope.selectedOption = "All";
-            var getUsersUrl = apiUrl + 'getUsers';
+            var getHelpUrl = apiUrl + 'getHelpcontent';
              var config = {
                     headers : {
                         'Content-Type': 'application/json'
                         }
                 }
-            $http.get(getUsersUrl, config)
+            $http.get(getHelpUrl, config)
                 .then(function (data, status, config) {
-                    var users = angular.fromJson(angular.toJson(data));
-                    angular.forEach(users.data, function(user){
-                       user.firstname = user.firstname.trim();
-                       user.lastname = user.lastname.trim();
-                       user.email = user.email.trim();
-                       user.phone = user.phone.trim();
-                       user.isEdit = false; 
+                    var Help = angular.fromJson(angular.toJson(data));
+                    angular.forEach(Help.data, function(Help){
+                       Help.title = Help.title.trim();
+                       Help.discription = Help.discription.trim();
                     });
-                    $scope.userlist = angular.copy(users.data);
-                    $scope.usersMainList = angular.copy(users.data);
+                    $scope.Helplist = angular.copy(Help.data);
+                    $scope.HelpMainList = angular.copy(Help.data);
                 })
                 .catch(function (data, status, config) {
                     console.log(data);
-                    alert('failed to retreive users list');
+                    alert('failed to retreive Help list');
                 });
         };
     
-        $scope.redirectToEdit = function(userInfo){
-            $state.go('createuser', {'userdata':userInfo, 'mode':'E'});
+        $scope.redirectToEdit = function(HelpInfo){
+            $state.go('createhelp', {'helpdata':userInfo, 'mode':'E'});
         }
         
-        $scope.updateStatus = function(userInfo) {
-            if(angular.isDefined(userInfo) && userInfo !== null) {
-                var config = {
-                    headers : {
-                        'Content-Type': 'application/json'
-                        }
-                }
-                
-                var updateStatusurl = apiUrl + 'updateStatus?id=\''+userInfo.id+'\'';
-                console.log(updateStatusurl);
-                
-                $http.put(updateStatusurl, null, config)
-                    .then(function (data, status, headers, config) {
-                        $scope.getUsers();
-                    })
-                    .catch(function (data, status, header, config) {
-                        console.log(data);
-                    });
-            }
-        };
         
-        $scope.updateUser = function(userInfo) {
-            if(angular.isDefined(userInfo) && userInfo !== null) {
+        $scope.updateHelp = function(HelpInfo) {
+            if(angular.isDefined(HelpInfo) && HelpInfo !== null) {
                 var config = {
                     headers : {
                         'Content-Type': 'application/json'
                         }
                 }
                 
-                var updateStatusurl = apiUrl + 'updateUserInfo';
-                console.log(updateStatusurl);
+                var updateHelpurl = apiUrl + 'updateHelpInfo';
+                console.log(updateHelpurl);
                 
-                $http.post(updateStatusurl, userInfo, config)
+                $http.post(updateHelpurl, HelpInfo, config)
                     .then(function (data, status, headers, config) {
-                        $scope.getUsers();
+                        $scope.getHelp();
                     })
                     .catch(function (data, status, header, config) {
                         console.log(data);
@@ -77,23 +54,6 @@ app.controller("userCtrl", function($scope, $http, $state, apiUrl) {
             }
         };
     
-        $scope.filterUsers = function(option){
-            $scope.userlist = [];
-            if(option==="All"){
-                $scope.userlist = angular.copy($scope.usersMainList);
-            }else if(option==="Active"){
-                angular.forEach($scope.usersMainList,function(user){
-                    if(user.active===true){
-                        $scope.userlist.push(user);
-                    }
-                });
-            }else if(option==="InActive"){
-                angular.forEach($scope.usersMainList,function(user){
-                    if(user.active===false){
-                        $scope.userlist.push(user);
-                    }
-                });
-            }
-        }
+        
     });    
 })();
