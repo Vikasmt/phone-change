e8cbb80e3b98d1896be6a70f1dd66afb2c18b14f
vikas <vikas.y@moldtekindia.com>
2017-04-04 12:58:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -340,25 +340,7 @@ router.get('/getDisclaimercontent', function(req, res) {
     });
 });
 
-
 router.get('/getHelpcontent', function(req, res) {
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-        if (err) console.log(err);
-        conn.query(
-            'SELECT id, title, discription, isexpand from Helptable',
-            function(err,result){
-                done();
-                if(err){
-                    return res.status(400).json({error: err.message});
-                }
-                else{
-                    return res.json(result.rows);
-                }
-            });
-    });
-});
-
-router.get('/getHelpdata', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         if (err) console.log(err);
         conn.query(
