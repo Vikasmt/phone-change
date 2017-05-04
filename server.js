5fe0166b79117fbebaada958477a24bc411459d3
vikas <vikas.y@moldtekindia.com>
2017-05-04 19:11:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1095,8 +1095,8 @@ router.post('/insertNeedleIssue', function(req, res) {
 	 
         var combinedQuery = insertQueryData + ')' + valuesData + ') RETURNING id';
         console.log('............combinedQuery.............'+combinedQuery); 
-        var temp = combinedQuery.replace(/'/g,'"');    
-	combinedQuery = temp.replace("%2527","'");    
+        var temp = combinedQuery;    
+	combinedQuery = temp.replace("%2527","\'");    
 	console.log('--------------------------------temp-------------------------------'+combinedQuery);
         conn.query(combinedQuery,
                 function(err, result) {
