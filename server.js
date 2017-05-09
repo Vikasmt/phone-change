2a0e15c70b0db3915d0ef04803a1198650a0fb33
vikas <vikas.y@moldtekindia.com>
2017-05-09 17:42:33 +0530

    Update

--- server.js
+++ server.js
@@ -2160,7 +2160,7 @@ router.put('/changePassword', function(req, res) {
         
         var userManagementQueryStr = 'Update UserManagement set password=\''+newPassword+'\' where id='+user_id+' and password=\''+oldPassword+'\'';
         console.log(userManagementQueryStr);
-        
+       
         conn.query('SELECT *from UserManagement where id='+user_id+'',
             function(err,result){
                 if(err){
@@ -2176,22 +2176,12 @@ router.put('/changePassword', function(req, res) {
                                 return res.status(400).json({error: err.message});
                             }
                             else{
-                                var contactId = result.rows[0].contactid;
-				console.log('contactId:'+contactId);
-								
-				conn.query('Update salesforce.contact set IVOPPassword__c=\''+newPassword+'\' where id='+contactId+' and IVOPPassword__c=\''+oldPassword+'\'', 
-				function(err,result){
-				if(err){
-					return res.status(400).json({error: err.message});
-				}
-				else{
-				     return res.status(200).json({
-				                                  msgid: 1,
-								  message: 'Success.'});
-				}
+                                return res.status(200).json({
+                                            msgid: 1,
+                                            message: 'Success.'});
                             }
                     });
-		}
+                }
             });
     });
 });
