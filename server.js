5d1d00679cafc2cb70fa1de0dcba68f14b545d1e
vikas <vikas.y@moldtekindia.com>
2017-06-06 16:40:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -938,7 +938,8 @@ var caseid = req.param('caseid');
 var Description = req.param('DefectDescription'); 	
          pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
              if (err) console.log(err);
-             conn.query('SELECT *from salesforce.Case WHERE id='+caseid+'',
+	     var jsonData = req.body;
+             conn.query('SELECT *from salesforce.Case WHERE id='+jsonData.caseid+'',
                 function(err,result){
                  if (err != null || result.rowCount == 0) {
                       return res.json({
@@ -946,7 +947,7 @@ var Description = req.param('DefectDescription');
                                 message: 'case id not found.'});
                          }else{
 			        var Des = 'Description';
-                                conn.query('UPDATE salesforce.Case SET '+Des+' = \''+Description+'\' WHERE id='+caseid+'',
+                                conn.query('UPDATE salesforce.Case SET '+Des+' = \''+jsonData.Description+'\' WHERE id='+jsonData.caseid+'',
                                     function(err,result){
                                         done();
                                         if(err){
