6d7c75bc8cc3cf9164f5c767f25d72442de00405
vikas <vikas.y@moldtekindia.com>
2017-06-07 11:56:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2564,7 +2564,7 @@ router.post('/CreateUser', function(req, res) {
     console.log(req.body);
     var jsonData = req.body;
     
-    var formattedData='INSERT INTO Salesforce.Contact (firstname, lastname, email, phone, IVOPPassword__c, IVOPMobileappuser__c) VALUES (\''+jsonData.firstname+'\', \''+jsonData.lastname+'\', \''+jsonData.email+'\', \''+jsonData.phone+'\', \''+jsonData.password+'\', \''+'True'+'\')  RETURNING id';
+    var formattedData='INSERT INTO Salesforce.Contact (firstname, lastname, email, phone, IVOPPassword__c, IVOPMobileappuser__c, IVOP_AppUserCountry__c) VALUES (\''+jsonData.firstname+'\', \''+jsonData.lastname+'\', \''+jsonData.email+'\', \''+jsonData.phone+'\', \''+jsonData.password+'\', \''+'True'+'\', \''+jsonData.country+'\')  RETURNING id';
     console.log('formatted Salesforce.Contact Query:'+formattedData);
     
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
@@ -2578,7 +2578,7 @@ router.post('/CreateUser', function(req, res) {
                     res.status(400).json({error: 'Email already exist.'});
                 }
                  else{
-                    conn.query('INSERT INTO Salesforce.Contact (firstname, lastname, email, phone, IVOPPassword__c, IVOPMobileappuser__c, MailingCountry) VALUES (\''+jsonData.firstname+'\', \''+jsonData.lastname+'\', \''+jsonData.email.toLowerCase().trim()+'\', \''+jsonData.phone+'\', \''+jsonData.password+'\', \''+'True'+'\', \''+jsonData.country+'\')  RETURNING id',
+                    conn.query('INSERT INTO Salesforce.Contact (firstname, lastname, email, phone, IVOPPassword__c, IVOPMobileappuser__c, IVOP_AppUserCountry__c) VALUES (\''+jsonData.firstname+'\', \''+jsonData.lastname+'\', \''+jsonData.email.toLowerCase().trim()+'\', \''+jsonData.phone+'\', \''+jsonData.password+'\', \''+'True'+'\', \''+jsonData.country+'\')  RETURNING id',
                          function(err, result) {
                             if(err){
                                     return res.json({
