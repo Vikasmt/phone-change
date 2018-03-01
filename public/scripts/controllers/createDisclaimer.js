23afa10e3ae22917bd45d7e24e66e490d658ae17
Suneel <suneelks@proarch.com>
2018-03-01 16:10:23 +0530

    Disclaimer changes done

--- public/scripts/controllers/createDisclaimer.js
+++ public/scripts/controllers/createDisclaimer.js
@@ -1,41 +1,40 @@
-(function(){
-var app = angular.module("mainApp");
-      
+(function() {
+    var app = angular.module("mainApp");
 
 
-app.controller("createDisclaimerCtrl", function($scope, $http, $state, $stateParams, apiUrl) {
-  
-        $scope.disclaimer={};
- 
-      
-          $scope.bindParameters = function(){
+
+    app.controller("createDisclaimerCtrl", function($scope, $http, $state, $cookieStore, $stateParams, apiUrl) {
+
+        $scope.disclaimer = {};
+
+        $scope.bindParameters = function() {
             $scope.disclaimer = angular.isDefined($stateParams.disclaimerdata) ? $stateParams.disclaimerdata : {};
             console.log($scope.disclaimer);
             $scope.mode = angular.isDefined($stateParams.mode) ? $stateParams.mode : 'C';
             console.log($scope.mode);
-          }
-    
+        }
+
         $scope.CreateDisclaimer = function(disclaimerInformation) {
-                
-            if(angular.isDefined(disclaimerInformation) && disclaimerInformation !== null) {
+            if (angular.isDefined(disclaimerInformation) && disclaimerInformation !== null) {
                 var config = {
-                    headers : {
-                        'Content-Type': 'application/json'
-                        }
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'token': $cookieStore.get('AccessToken')
+                    }
                 }
-                
-                var createdisclaimerurl = apiUrl + ($scope.mode==='E' ? 'updateDisclaimerInfo' : 'CreateDisclaimer');
+
+                var createdisclaimerurl = apiUrl + ($scope.mode === 'E' ? 'updateDisclaimerInfo' : 'CreateDisclaimer');
                 console.log(createdisclaimerurl);
-                
-                $http.post(createdisclaimerurl,disclaimerInformation,config)
-                    .then(function (data, status, headers, config) {
-                        $scope.disclaimer={};
+
+                $http.post(createdisclaimerurl, disclaimerInformation, config)
+                    .then(function(data, status, headers, config) {
+                        $scope.disclaimer = {};
                         $state.go('disclaimerlist');
                     })
-                    .catch(function (data, status, header, config) {
+                    .catch(function(data, status, header, config) {
                         console.log(data);
                     });
             }
         };
-    });    
+    });
 })();
--- public/scripts/controllers/disclaimerlist.js
+++ public/scripts/controllers/disclaimerlist.js
@@ -1,59 +1,64 @@
-(function(){
-var app = angular.module("mainApp");
-app.controller("DisclaimerCtrl", function($scope, $http, $state, apiUrl) {
+(function() {
+    var app = angular.module("mainApp");
+    app.controller("DisclaimerCtrl", function($scope, $http, $state, $cookieStore, apiUrl) {
         $scope.helplist = [];
         $scope.helpMainList = [];
-    
-        $scope.getDisclaimercontent = function () {
+
+        $scope.getDisclaimercontent = function() {
             var getDisclaimerUrl = apiUrl + 'getDisclaimercontent';
-             var config = {
-                    headers : {
-                        'Content-Type': 'application/json'
-                        }
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': $cookieStore.get('AccessToken')
                 }
+            }
             $http.get(getDisclaimerUrl, config)
-                .then(function (data, status, config) {
+                .then(function(data, status, config) {
                     var Disclaimer = angular.fromJson(angular.toJson(data));
-                    angular.forEach(Disclaimer.data, function(Disclaimer){
-                       Disclaimer.country = Disclaimer.country.trim();
-                       Disclaimer.language = Disclaimer.language.trim();
-                       Disclaimer.content = Disclaimer.content.trim();
+                    angular.forEach(Disclaimer.data, function(Disclaimer) {
+                        Disclaimer.country = Disclaimer.country !== null ? Disclaimer.country.trim() : '';
+                        Disclaimer.language = Disclaimer.language !== null ? Disclaimer.language.trim() : '';
+                        Disclaimer.content = Disclaimer.content !== null ? Disclaimer.content.trim() : '';
                     });
                     $scope.Disclaimerlist = angular.copy(Disclaimer.data);
                     $scope.DisclaimerMainList = angular.copy(Disclaimer.data);
                 })
-                .catch(function (data, status, config) {
+                .catch(function(data, status, config) {
                     console.log(data);
                     alert('failed to retreive Help list');
                 });
         };
-    
-        $scope.redirectToEdit = function(DisclaimerInfo){
-            $state.go('createDisclaimer', {'disclaimerdata':DisclaimerInfo, 'mode':'E'});
+
+        $scope.redirectToEdit = function(DisclaimerInfo) {
+            $state.go('createDisclaimer', {
+                'disclaimerdata': DisclaimerInfo,
+                'mode': 'E'
+            });
         }
-        
-        
+
+
         $scope.updateDisclaimer = function(DisclaimerInfo) {
-            if(angular.isDefined(DisclaimerInfo) && DisclaimerInfo !== null) {
+            if (angular.isDefined(DisclaimerInfo) && DisclaimerInfo !== null) {
                 var config = {
-                    headers : {
-                        'Content-Type': 'application/json'
-                        }
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'token': $cookieStore.get('AccessToken')
+                    }
                 }
-                
+
                 var updateDisclaimerurl = apiUrl + 'updateDisclaimerInfo';
                 console.log(updateDisclaimerurl);
-                
+
                 $http.post(updateDisclaimerurl, DisclaimerInfo, config)
-                    .then(function (data, status, headers, config) {
+                    .then(function(data, status, headers, config) {
                         $scope.getDisclaimercontent();
                     })
-                    .catch(function (data, status, header, config) {
+                    .catch(function(data, status, header, config) {
                         console.log(data);
                     });
             }
         };
-    
-        
-    });    
+
+
+    });
 })();
