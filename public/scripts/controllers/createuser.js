6647b649c2e5a03c306fdfa449f9d74f7dcd9605
Suneel <sunilsvsnlr@gmail.com>
2018-03-08 07:10:30 +0530

    handling error messages in ui

--- public/scripts/controllers/createuser.js
+++ public/scripts/controllers/createuser.js
@@ -1,14 +1,14 @@
-(function() {
+(function () {
     var app = angular.module("mainApp");
 
 
 
-    app.controller("createCtrl", function($scope, $http, $state, $cookieStore, $stateParams, apiUrl) {
+    app.controller("createCtrl", function ($scope, $http, $state, $cookieStore, $stateParams, apiUrl) {
 
         $scope.user = {};
 
 
-        $scope.bindParameters = function() {
+        $scope.bindParameters = function () {
             $scope.user = angular.isDefined($stateParams.userdata) ? $stateParams.userdata : {};
             console.log($scope.user);
             $scope.mode = angular.isDefined($stateParams.mode) ? $stateParams.mode : 'C';
@@ -17,7 +17,7 @@
             $scope.country = ["Denmark", "Ecuador", "Egypt", "El Salvador", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Herzegovina", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Italy", "Ivory Coast", "Japan", "Jordan", "Kazakhstan", "Kenya", "South Korea", "Kuwait", "Laos", "Latvia", "Lebanon", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Malaysia", "Maldives", "Mali", "Mauritius", "Malta", "Mexico", "Moldova", "Monaco", "Mongolia", "Morocco", "Mozambique", "Myanmar", "Namibia", "Netherlands", "New Caledonia", "Nepal", "New Zealand", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Panama", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Sweden", "Switzerland", "Syria", "Taiwan", "Tanzania", "Thailand", "Tunisia", "Turkey", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "US", "Uzbekistan", "Vatican City St.,Venezuela", "Vietnam", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahrain", "Bangladesh", "Belarus", "Belgium", "Bolivia", " Plurinational State of", "Bosnia and Herzegovina", "Brazil", "Bulgaria", "Cameroon", "Canada", "Chad", "Chile", "China", "Columbia", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Barbados", "Antarctica", "Aland Islands", "Botswana", "Benin", "Bonaire Sint Eustatius and Saba", "British Indian Ocean Territory", "Bhutan", "Bouvet Island", "Antigua and Barbuda", "Belize", "Bahamas", "Bermuda"];
         }
 
-        $scope.createUser = function(userInformation) {
+        $scope.createUser = function (userInformation) {
             if (angular.isDefined(userInformation) && userInformation !== null) {
                 var config = {
                     headers: {
@@ -30,17 +30,24 @@
                 console.log(createuserurl);
 
                 $http.post(createuserurl, userInformation, config)
-                    .then(function(data, status, headers, config) {
-                        $scope.user = {};
-                        $state.go('userlist');
+                    .then(function (data, status, headers, config) {
+                        if (data.data.msgid === 1) {
+                            $scope.user = {};
+                            $state.go('userlist');
+                        } else {
+                            alert(data.data.message);
+                        }
                     })
-                    .catch(function(data, status, header, config) {
+                    .catch(function (data, status, header, config) {
+                        if (data.status === 400) {
+                            alert(data.data.error);
+                        }
                         console.log(data);
                     });
             }
         };
 
-        $scope.validateForm = function(myform) {
+        $scope.validateForm = function (myform) {
             if ($scope.mode === 'E') {
                 return myForm.firstname.$invalid ||
                     myForm.lastname.$invalid ||
